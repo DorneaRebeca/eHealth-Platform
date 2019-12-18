1cf4546d9d58409dcb8ee345ab78a4be46b3ac73
unknown <rebbe1997dornea@gmail.com>
2019-12-18 00:20:45 -0800

    add measurements tutorial page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import {HomeComponent  as HomePatientComponent} from "./patient/home/home.component";
 import {HomeComponent as HomeDoctorComponent} from "./doctor/home/home.component"
+import {MeasurementsTutorialComponent} from './patient/measurements-tutorial/measurements-tutorial.component';
 
 const routes: Routes = [
     { path: 'login', component: LoginComponent } ,
     { path: 'home-patient', component: HomePatientComponent },
-    { path: 'home-doctor', component: HomeDoctorComponent }
+    { path: 'home-doctor', component: HomeDoctorComponent },
+    { path: 'measurements-tutorial', component: MeasurementsTutorialComponent },
 ];
 
 @NgModule({
