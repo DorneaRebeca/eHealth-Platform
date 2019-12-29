95e943cb3c0635d8e3b1966dc42d6e8ea1c89b04
Serban Coroiu <serban.coroiu@halcyonmobile.com>
2019-12-29 18:36:29 +0200

    added medical history table

--- src/app/doctor/patient-detail/patient-detail.component.ts
+++ src/app/doctor/patient-detail/patient-detail.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import {DoctorService} from "../../services/doctor.service";
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-patient-detail',
   templateUrl: './patient-detail.component.html',
-  styleUrls: ['./patient-detail.component.css']
+  styleUrls: ['./patient-detail.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 
 export class PatientDetailComponent implements OnInit {
@@ -14,8 +22,11 @@ export class PatientDetailComponent implements OnInit {
     ['Height', '191 cm'],
     ['Weigth', '86 kg'],
   ];
-
   patientActivities = [];
+  dataSource = ELEMENT_DATA;
+  columnsToDisplay = ['name', 'start_date', 'duration'];
+  expandedElement: Record | null;
+
   constructor(private doctorService: DoctorService) { }
 
   ngOnInit() {
@@ -23,3 +34,73 @@ export class PatientDetailComponent implements OnInit {
   }
 
 }
+
+export interface Record {
+  name: string;
+  start_date: string;
+  duration: number;
+  description: string;
+}
+
+const ELEMENT_DATA: Record[] = [
+  {
+    name: 'Astm',
+    start_date: '17-12-2019',
+    duration: 15,
+    description: 'Pacientul s-a prezentat cu stari febrile(39,8 C). I-au fost administrate calmante, iar in urma analizelor s-a observat un nivel ridicat de celule albe ceea ce a dus la o investigatie mai amanuntita.'
+    },
+  {
+    name: 'Faringita',
+    start_date: '17-12-2019',
+    duration: 15,
+    description: ''
+  },
+  {
+    name: 'Laringita>',
+    start_date: '17-12-2019',
+    duration: 15,
+    description: ''
+  },
+  {
+    name: 'Bronsita',
+    start_date: '17-12-2019',
+    duration: 15,
+    description: ''
+  },
+  {
+    name: 'Pneumonie',
+    start_date: '17-12-2019',
+    duration: 15,
+    description: ''
+  },
+  {
+    name: 'Pneumonie',
+    start_date: '17-12-2019',
+    duration: 15,
+    description: ''
+  },
+  {
+    name: 'Pneumonie',
+    start_date: '17-12-2019',
+    duration: 15,
+    description: ''
+  },
+  {
+    name: 'Pneumonie',
+    start_date: '17-12-2019',
+    duration: 15,
+    description: ''
+  },
+  {
+    name: 'Pneumonie',
+    start_date: '17-12-2019',
+    duration: 15,
+    description: ''
+  },
+  {
+    name: 'Pneumonie',
+    start_date: '17-12-2019',
+    duration: 15,
+    description: ''
+  },
+];
--- src/app/services/doctor.service.ts
+++ src/app/services/doctor.service.ts
@@ -8,24 +8,35 @@ export class DoctorService {
 
   constructor() { }
 
+  getPatients() {
+    var patients = [];
+    patients.push({ name: 'Pop Ionel', age: '34 years old', gender: 'Male', height: '191 cm', weigth: '86 kg'});
+    patients.push({ name: 'Dan Tudor', age: '28 years old', gender: 'Male', height: '192 cm', weigth: '100 kg'});
+    patients.push({ name: 'Palade Adrian', age: '31 years old', gender: 'Male', height: '195 cm', weigth: '98 kg'});
+    patients.push({ name: 'Dan Andreea', age: '25 years old', gender: 'Female', height: '178 cm', weigth: '56 kg'});
+    patients.push({ name: 'Dornea Rebeca', age: '29 years old', gender: 'Female', height: '169 cm', weigth: '61 kg'});
+    return patients;
+  }
+
   getDetailsForPatient(name) {
     var activities = [];
-    if(name == 'Pop Ionel') {
-      activities.push({created_at: "2019-12-21 17:51", duration: "7 min", distance:"0,3 km", title:"Shopping Walk", image: "directions_walk"});
-      activities.push({created_at: "2019-12-21 17:51", duration: "7 min", distance:"0,3 km", title:"Shopping Walk", image: "directions_run"});
-      activities.push({created_at: "2019-12-21 17:51", duration: "7 min", distance:"0,3 km", title:"Shopping Walk", image: "directions_run"});
-      activities.push({created_at: "2019-12-21 17:51", duration: "7 min", distance:"0,3 km", title:"Shopping Walk", image: "directions_run"});
-      activities.push({created_at: "2019-12-21 17:51", duration: "7 min", distance:"0,3 km", title:"Shopping Walk", image: "directions_bike"});
-      activities.push({created_at: "2019-12-21 17:51", duration: "7 min", distance:"0,3 km", title:"Shopping Walk", image: "directions_bike"});
-      activities.push({created_at: "2019-12-21 17:51", duration: "7 min", distance:"0,3 km", title:"Shopping Walk", image: "directions_run"});
-      activities.push({created_at: "2019-12-21 17:51", duration: "7 min", distance:"0,3 km", title:"Shopping Walk", image: "directions_walk"});
-      activities.push({created_at: "2019-12-21 17:51", duration: "7 min", distance:"0,3 km", title:"Shopping Walk", image: "directions_bike"});
-      activities.push({created_at: "2019-12-21 17:51", duration: "7 min", distance:"0,3 km", title:"Shopping Walk", image: "directions_walk"});
-      activities.push({created_at: "2019-12-21 17:51", duration: "7 min", distance:"0,3 km", title:"Shopping Walk", image: "directions_walk"});
-      activities.push({created_at: "2019-12-21 17:51", duration: "7 min", distance:"0,3 km", title:"Shopping Walk", image: "directions_walk"})
+    if (name === 'Pop Ionel') {
+      activities.push({created_at: '2019-12-21 17:51', duration: '17 min', distance: '0,5 km', title: 'Short Run', image: 'run'});
+      activities.push({created_at: '2019-12-21 17:51', duration: '7 min', distance: '0,3 km', title: 'Shopping Walk', image: 'walk'});
+      activities.push({created_at: '2019-12-21 17:51', duration: '25 min', distance: '0,65 km', title: 'Late Swiming', image: 'swim'});
+      activities.push({created_at: '2019-12-21 17:51', duration: '77 min', distance: '0,89 km', title: 'Shopping Run', image: 'run'});
+      activities.push({created_at: '2019-12-21 17:51', duration: '42 min', distance: '0,46 km', title: 'Afternoon Walk', image: 'walk'});
+      activities.push({created_at: '2019-12-21 17:51', duration: '45 min', distance: '1,3 km', title: 'Afternoon Biking', image: 'bike'});
+      activities.push({created_at: '2019-12-21 17:51', duration: '62 min', distance: '0,1 km', title: 'Morning Yoga Session', image: 'yoga'});
+      activities.push({created_at: '2019-12-21 17:51', duration: '34 min', distance: '1,2 km', title: 'Midnight run', image: 'run'});
+      activities.push({created_at: '2019-12-21 17:51', duration: '23 min', distance: '0,6 km', title: 'Late Walk', image: 'walk'});
     }
+    return activities;
+  }
 
-    return activities
+
+  getMedicalHistory() {
+
   }
 }
 
@@ -34,5 +45,13 @@ export interface Activity {
   duration: string;
   distance: string;
   title: string;
-  image: string
+  image: string;
+}
+
+export interface Patient {
+  name: string;
+  age: string;
+  gender: string;
+  height: string;
+  weigth: string;
 }
