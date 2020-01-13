0b335ab60bcf3a8e12f6d754d4e3daf8975c4f57
unknown <rebbe1997dornea@gmail.com>
2020-01-13 23:48:59 +0200

    add measurements validations

--- src/app/patient/measurements/measurements.component.ts
+++ src/app/patient/measurements/measurements.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {MatDialogRef, MatExpansionPanel} from '@angular/material';
+import {MatDialogRef, MatExpansionPanel, MatSnackBar} from '@angular/material';
 import {PatientService} from '../../services/patient.service';
-import {FormControl, Validators} from '@angular/forms';
+import {FormControl, FormGroup, ValidationErrors, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
 
 
@@ -24,17 +24,39 @@ export class MeasurementsComponent implements OnInit {
   sugarDate : Date =  new  Date();
   bloodSugar : number;
 
-  systolicControl = new FormControl('', [Validators.required, Validators.max(999)]);
-  diastolicControl = new FormControl('', [Validators.required, Validators.max(999)]);
-  pulseControl = new FormControl('', [Validators.required, Validators.max(999)]);
-  sugarControl = new FormControl('', [Validators.required, Validators.max(999)]);
-  tempControl = new FormControl('', [Validators.required, Validators.max(99)]);
+  public bloodPressureForm = new FormGroup({
+    systolicControl: new FormControl('', [Validators.required, Validators.max(999)]),
+  diastolicControl : new FormControl('', [Validators.required, Validators.max(999)]),
+  pulseControl : new FormControl('', [Validators.required, Validators.max(999)]),
+      pressureDateControl: new FormControl('', [
+        Validators.required,
+      ]),
+}
+  );
+
+  public sugarForm = new FormGroup({
+    sugarControl : new FormControl('', [Validators.required, Validators.max(999)]),
+    sugarDateControl: new FormControl('', [
+      Validators.required,
+    ]),
+
+});
+  public temperatureForm = new FormGroup(
+    {
+      tempControl: new FormControl('', [Validators.required, Validators.max(99)]),
+      tempDateControl: new FormControl('', [
+        Validators.required,
+      ]),
+
+}
+  );
 
   @ViewChild('panel1', {static : false}) firstPanel: MatExpansionPanel;
   @ViewChild('panel2', {static : false}) secondPanel: MatExpansionPanel;
   @ViewChild('panel3', {static : false}) thirdPanel: MatExpansionPanel;
 
   constructor(
+    private snackBar: MatSnackBar,
     private dialogRef: MatDialogRef<MeasurementsComponent>,
     private patientService : PatientService,
     private route : Router,
@@ -49,37 +71,44 @@ export class MeasurementsComponent implements OnInit {
   }
 
   saveTemperature() {
-    let tempData = {
-      date : this.tempDate,
-      temperature : this.temperature
-    };
-    this.showLoadingScreen(1000);
-    this.patientService.saveDataInLocalStorageArray(tempData, 'temperatureData');
-    this.secondPanel.close();
+    if(!this.validationErrorsExistsTemp()) {
+      let tempData = {
+        date: this.tempDate,
+        temperature: this.temperature
+      };
+      this.patientService.saveDataInLocalStorageArray(tempData, 'temperatureData');
+      this.showLoadingScreen(1000);
+      this.secondPanel.close();
+    }
   }
 
   saveBloodSugar() {
-    let sugarData =
+    if(!this.validationErrorsExistsSugar()){
+      let sugarData =
       {
         date : this.sugarDate,
         sugar : this.bloodSugar
   };
-    this.showLoadingScreen(1000);
     this.patientService.saveDataInLocalStorageArray(sugarData, 'sugarData');
+    this.showLoadingScreen(1000);
     this.thirdPanel.close();
   }
 
-  saveBloodPressure(){
-    let pressureData = {
-      date : this.presDate,
-      systolic :this.systolic,
-      diastolic : this.diastolic,
-      pulseRate : this.pulse
-    };
-    this.showLoadingScreen(1000);
-    this.patientService.saveDataInLocalStorageArray(pressureData, 'pressureData');
-    this.firstPanel.close();
+  }
 
+  saveBloodPressure(){
+    if(!this.validationErrorsExistsPressure()) {
+      let pressureData = {
+        date: this.presDate,
+        systolic: this.systolic,
+        diastolic: this.diastolic,
+        pulseRate: this.pulse
+      };
+      this.patientService.saveDataInLocalStorageArray(pressureData, 'pressureData');
+      console.log(localStorage.getItem('pressureData'));
+      this.showLoadingScreen(1000);
+      this.firstPanel.close();
+    }
   }
 
   onClear() {
@@ -107,6 +136,38 @@ export class MeasurementsComponent implements OnInit {
      this.route.navigateByUrl('measurements-tutorial');
    }
     this.onClose();
+  }
+
+  validationErrorsExistsPressure(): boolean {
+    var noOfErrors: number = 0;
+    Object.keys(this.bloodPressureForm.controls).forEach(key => {
+      const controlErrors: ValidationErrors = this.bloodPressureForm.get(key).errors;
+      if (controlErrors != null) {
+        noOfErrors++;
+      }
+    });
+    return noOfErrors > 0;
+  }
+  validationErrorsExistsTemp(): boolean {
+    var noOfErrors: number = 0;
+    Object.keys(this.temperatureForm.controls).forEach(key => {
+      const controlErrors: ValidationErrors = this.temperatureForm.get(key).errors;
+      if (controlErrors != null) {
+        noOfErrors++;
+      }
+    });
+
+    return noOfErrors > 0;
+  }
+  validationErrorsExistsSugar(): boolean {
+    var noOfErrors: number = 0;
+    Object.keys(this.sugarForm.controls).forEach(key => {
+      const controlErrors: ValidationErrors = this.sugarForm.get(key).errors;
+      if (controlErrors != null) {
+        noOfErrors++;
+      }
+    });
+    return noOfErrors > 0;
   }
 
 }
